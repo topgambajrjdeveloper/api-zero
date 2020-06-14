391204c84edcdecb5b51c6ddc68c04682e2f710d
topgambajrjdeveloper <topgambajrjdeveloper@gmail.com>
2020-06-14 20:00:56 +0200

    ajustes para deploy

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 // arrancar la app
-app.listen(port, host, () => {
+app.listen(port, '0.0.0.0',() => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
